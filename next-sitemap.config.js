0d7f3c8ccc2f4547ce12d158bfcf8a1c9d895e1a
Andri kajawi <andrikajawi@gmail.com>
2025-07-19 10:53:35 +0000

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://kampungkaos.com', // ganti jika berbeda
+    generateRobotsTxt: true,
+    sitemapSize: 5000,
+  };
+  
